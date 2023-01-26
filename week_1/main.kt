9de75fe6a24006c98b2482b8e28a34b04933c290
Faizan Ahmed <5ahmef94@solent.ac.uk>
2023-01-26 11:49:22 +0000

    Initial Kotlin file setup for doing activities related to week 1 of MAD.

---
+++ week_1/main.kt
@@ -0,0 +1,12 @@
+MAD - Mobile Application Developement COM527
+/*
+Activity file for the week 1 exercises of the module Mobile Application Development (COM527)
+*/
+
+//Question 1 : Doing the Hello World example from the notes
+/*
+fun main(args: Array<String>) {
+	println("Hello World!")
+}
+*/
+

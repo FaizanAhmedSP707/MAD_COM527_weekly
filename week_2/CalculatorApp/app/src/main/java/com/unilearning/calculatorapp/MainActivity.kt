363146414e75bc65cc64bbd391f3f0768ef33552
Faizan Ahmed <5ahmef94@solent.ac.uk>
2023-02-01 12:35:23 +0000

    Added 2 text fields and 4 buttons that carry out the functions of a basic calculator; need to add the relevant IDs to the respective layout file.

--- week_2/CalculatorApp/app/src/main/java/com/unilearning/calculatorapp/MainActivity.kt
+++ week_2/CalculatorApp/app/src/main/java/com/unilearning/calculatorapp/MainActivity.kt
@@ -16,6 +16,36 @@ class MainActivity : AppCompatActivity() {
         val btn2 = findViewById<Button>(R.id.subtractBtn)
         val btn3 = findViewById<Button>(R.id.multiplyBtn)
         val btn4 = findViewById<Button>(R.id.divideBtn)
-        //val numTextField1 = find
+        val numTextField1 = findViewById<EditText>(R.id.numfield1)
+        val numTextField2 = findViewById<EditText>(R.id.numfield2)
+        val textViewAnswer = findViewById<TextView>(R.id.tvAnswer)
+
+        btn1.setOnClickListener {
+            val number1 = numTextField1.getText().toString().toDouble()
+            val number2 = numTextField2.getText().toString().toDouble()
+            val addAnswer = number1+number2
+            textViewAnswer.setText("Your two numbers added together gives: $addAnswer")
+        }
+
+        btn2.setOnClickListener {
+            val number1 = numTextField1.getText().toString().toDouble()
+            val number2 = numTextField2.getText().toString().toDouble()
+            val subtractAnswer = number1-number2
+            textViewAnswer.setText("Your two numbers subtracted gives: $subtractAnswer")
+        }
+
+        btn3.setOnClickListener {
+            val number1 = numTextField1.getText().toString().toDouble()
+            val number2 = numTextField2.getText().toString().toDouble()
+            val multiplyAnswer = number1*number2
+            textViewAnswer.setText("Your two numbers multiplied together gives: $multiplyAnswer")
+        }
+
+        btn4.setOnClickListener {
+            val number1 = numTextField1.getText().toString().toDouble()
+            val number2 = numTextField2.getText().toString().toDouble()
+            val divisionAnswer = number1 / number2
+            textViewAnswer.setText("Your two numbers divided together give: $divisionAnswer")
+        }
     }
 }

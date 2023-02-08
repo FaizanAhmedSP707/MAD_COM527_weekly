184cb756ba40e81b395b199b2be75ab92bd86d1c
Faizan Ahmed <5ahmef94@solent.ac.uk>
2023-02-08 15:42:11 +0000

    Set up the project for completing exercise 3 of Week 3's activities.

---
+++ week_3/Exercise3_Map/app/src/main/java/com/unilearning/mapwithcoordinates/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.unilearning.mapwithcoordinates
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.preference.PreferenceManager
+import org.osmdroid.config.Configuration
+import org.osmdroid.util.GeoPoint
+import org.osmdroid.views.MapView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

47b098404bac62d19ec20355f5be974ad095eb8a
Faizan Ahmed <5ahmef94@solent.ac.uk>
2023-02-08 16:05:38 +0000

    Added all necessary UI elements to the app as well as some additional library imports.

--- week_3/Exercise3_Map/app/src/main/java/com/unilearning/mapwithcoordinates/MainActivity.kt
+++ week_3/Exercise3_Map/app/src/main/java/com/unilearning/mapwithcoordinates/MainActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.preference.PreferenceGroup
 import android.preference.PreferenceManager
+import android.widget.EditText
+import android.widget.TextView
 import org.osmdroid.config.Configuration
 import org.osmdroid.util.GeoPoint
 import org.osmdroid.views.MapView
@@ -15,6 +17,12 @@ class MainActivity : AppCompatActivity() {
         //Load necessary configuration so as to download the OSM maps.
         //This line sets the user agent, which is a requirement to download the OSM maps.
         Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this))
+
+        val latiText = findViewById<TextView>(R.id.textspace1)
+        val longiText = findViewById<TextView>(R.id.textspace2)
+        val latitudeField = findViewById<EditText>(R.id.latfield)
+        val longitudeField = findViewById<EditText>(R.id.lonfield)
+        val map1 = findViewById<MapView>(R.id.map1)
 
         setContentView(R.layout.activity_main)
     }

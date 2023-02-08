9085630e15ed15600c6b9e320cc4a09417207abe
Faizan Ahmed <5ahmef94@solent.ac.uk>
2023-02-08 15:52:08 +0000

    Added necessary configuration to download maps from OpenStreetMaps.

--- week_3/Exercise3_Map/app/src/main/java/com/unilearning/mapwithcoordinates/MainActivity.kt
+++ week_3/Exercise3_Map/app/src/main/java/com/unilearning/mapwithcoordinates/MainActivity.kt
@@ -2,6 +2,7 @@ package com.unilearning.mapwithcoordinates
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.preference.PreferenceGroup
 import android.preference.PreferenceManager
 import org.osmdroid.config.Configuration
 import org.osmdroid.util.GeoPoint
@@ -10,6 +11,11 @@ import org.osmdroid.views.MapView
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        //Load necessary configuration so as to download the OSM maps.
+        //This line sets the user agent, which is a requirement to download the OSM maps.
+        Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this))
+
         setContentView(R.layout.activity_main)
     }
 }

4737f3544bd225854ff13c7b4418df96bb84280e
Faizan Ahmed <5ahmef94@solent.ac.uk>
2023-02-11 14:39:15 +0000

    Commented out some unnecessary code and moved the 'setContentView()' function a couple of lines up to prevent issues with the app.

--- week_3/Exercise3_Map/app/src/main/java/com/unilearning/mapwithcoordinates/MainActivity.kt
+++ week_3/Exercise3_Map/app/src/main/java/com/unilearning/mapwithcoordinates/MainActivity.kt
@@ -5,7 +5,7 @@ import android.os.Bundle
 import android.preference.PreferenceManager
 import android.widget.Button
 import android.widget.EditText
-import android.widget.TextView
+//import android.widget.TextView
 import org.osmdroid.config.Configuration
 import org.osmdroid.util.GeoPoint
 import org.osmdroid.views.MapView
@@ -17,16 +17,16 @@ class MainActivity : AppCompatActivity() {
         //Load necessary configuration so as to download the OSM maps.
         //This line sets the user agent, which is a requirement to download the OSM maps.
         Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this))
+        setContentView(R.layout.activity_main)
 
-        val latiText = findViewById<TextView>(R.id.textspace1)
-        val longiText = findViewById<TextView>(R.id.textspace2)
+        val map1 = findViewById<MapView>(R.id.mapDisplay)
+        map1.controller.setZoom(14.0)
+        map1.controller.setCenter(GeoPoint(51.4934, 0.0098))
+        /*val latiText = findViewById<TextView>(R.id.textspace1)
+        val longiText = findViewById<TextView>(R.id.textspace2)*/
         val latitudeField = findViewById<EditText>(R.id.latfield)
         val longitudeField = findViewById<EditText>(R.id.lonfield)
         val submitBtn = findViewById<Button>(R.id.coordValsubmit)
-        val map1 = findViewById<MapView>(R.id.mapDisplay)
-
-        map1.controller.setZoom(15.0)
-        setContentView(R.layout.activity_main)
         
         submitBtn.setOnClickListener {
             if(latitudeField?.text.toString().trim() == ""){

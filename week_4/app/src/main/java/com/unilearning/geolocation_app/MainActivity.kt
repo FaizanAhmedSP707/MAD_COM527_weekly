4cb3ae98921ced0e6b69064432a82e97dbd6abdb
Faizan Ahmed <5ahmef94@solent.ac.uk>
2023-02-15 11:58:27 +0000

    Completed about 90% of the run-time permission checking, need to add the next appropriate function call.

--- week_4/app/src/main/java/com/unilearning/geolocation_app/MainActivity.kt
+++ week_4/app/src/main/java/com/unilearning/geolocation_app/MainActivity.kt
@@ -11,6 +11,10 @@ import android.location.LocationListener
 import android.location.Location
 import android.content.Context
 import android.widget.Toast
+import androidx.core.content.ContextCompat
+import android.Manifest
+import android.content.pm.PackageManager
+import androidx.core.app.ActivityCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,5 +30,33 @@ class MainActivity : AppCompatActivity() {
         map1.controller.setCenter(GeoPoint(0.00, 0.00))
 
         /* Now we need to check for what permissions have been granted!*/
+    }
+
+    fun checkPermissions() {
+        //Check to see if the necessary permission has been granted or not.
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) ==
+                PackageManager.PERMISSION_GRANTED) {
+            // TODO --> Add the function that opens the map in the app
+        } else {
+            // If the permissions haven't been granted, request them from the user
+            ActivityCompat.requestPermissions(this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION),
+                0)
+        }
+    }
+    /*
+    This method runs as soon as the user has either granted or denied the requested permission(s).
+    */
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when(requestCode) {
+            0 -> {
+                if(grantResults[0] == PackageManager.PERMISSION_GRANTED)
+            }
+        }
     }
 }

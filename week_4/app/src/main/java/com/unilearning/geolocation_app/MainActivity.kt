0c14e64d723f12be1b4a505db4ce252655988d9d
Faizan Ahmed <5ahmef94@solent.ac.uk>
2023-02-15 10:51:01 +0000

    Added basic configuration for loading the map.

--- week_4/app/src/main/java/com/unilearning/geolocation_app/MainActivity.kt
+++ week_4/app/src/main/java/com/unilearning/geolocation_app/MainActivity.kt
@@ -15,6 +15,16 @@ import android.widget.Toast
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        //Load necessary configuration so as to download the OSM maps.
+        //This line sets the user agent, which is a requirement to download the OSM maps.
+        Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this))
         setContentView(R.layout.activity_main)
+
+        val map1 = findViewById<MapView>(R.id.MapDisplay)
+        map1.controller.setZoom(14.0)
+        map1.controller.setCenter(GeoPoint(0.00, 0.00))
+
+        /* Now we need to check for what permissions have been granted!*/
     }
 }

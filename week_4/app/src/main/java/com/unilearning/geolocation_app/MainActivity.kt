324f921a63b4fd5e3592deb83df0858ca844ab32
Faizan Ahmed <5ahmef94@solent.ac.uk>
2023-02-15 10:35:20 +0000

    Added import statements that help with accessing a user's location.

--- week_4/app/src/main/java/com/unilearning/geolocation_app/MainActivity.kt
+++ week_4/app/src/main/java/com/unilearning/geolocation_app/MainActivity.kt
@@ -2,6 +2,11 @@ package com.unilearning.geolocation_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.location.LocationManager
+import android.location.LocationListener
+import android.location.Location
+import android.content.Context
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {

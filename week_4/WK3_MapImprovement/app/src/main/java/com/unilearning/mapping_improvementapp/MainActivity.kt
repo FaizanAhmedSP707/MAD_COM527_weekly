4a600c3bbf326b98ea7aafb4cfde8d041a95801f
Faizan Ahmed <5ahmef94@solent.ac.uk>
2023-02-17 14:35:03 +0000

    Added another inportant function that returns appropriate values elsewhere to functions that need it.

--- week_4/WK3_MapImprovement/app/src/main/java/com/unilearning/mapping_improvementapp/MainActivity.kt
+++ week_4/WK3_MapImprovement/app/src/main/java/com/unilearning/mapping_improvementapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.unilearning.mapping_improvementapp
 
 import android.Manifest
+import android.app.AlertDialog
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -34,6 +35,28 @@ class MainActivity : AppCompatActivity() {
             ActivityCompat.requestPermissions(this,
                 arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION),
                 0)
+        }
+    }
+
+    /*
+    This method runs as soon as the user has either granted or denied the requested permission(s).
+    */
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when(requestCode) {
+            0 -> {
+                if(grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // TODO --> Add the function that opens the map in the app
+                    requestLocation()
+                } else {
+                    AlertDialog.Builder(this).setPositiveButton("OK", null).
+                    setMessage("App will not be able to update the map efficiently.").show()
+                }
+            }
         }
     }
 }

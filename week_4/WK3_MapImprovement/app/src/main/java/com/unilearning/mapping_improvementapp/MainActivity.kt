946875db530ff2ee472f559726446ba5ff589d3b
Faizan Ahmed <5ahmef94@solent.ac.uk>
2023-02-17 14:30:36 +0000

    Added a function that checks for the granted permissions at run time.

--- week_4/WK3_MapImprovement/app/src/main/java/com/unilearning/mapping_improvementapp/MainActivity.kt
+++ week_4/WK3_MapImprovement/app/src/main/java/com/unilearning/mapping_improvementapp/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.unilearning.mapping_improvementapp
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.preference.PreferenceManager
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import org.osmdroid.config.Configuration
 import org.osmdroid.util.GeoPoint
 import org.osmdroid.views.MapView
@@ -18,5 +22,18 @@ class MainActivity : AppCompatActivity() {
         val map1 = findViewById<MapView>(R.id.map1)
         map1.controller.setZoom(14.0)
         map1.controller.setCenter(GeoPoint(51.05, -0.72))
+    }
+    fun checkPermissions() {
+        //Check to see if the necessary permission has been granted or not.
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) ==
+            PackageManager.PERMISSION_GRANTED) {
+            // TODO --> Add the function that opens the map in the app
+            requestLocation()
+        } else {
+            // If the permissions haven't been granted, request them from the user
+            ActivityCompat.requestPermissions(this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION),
+                0)
+        }
     }
 }

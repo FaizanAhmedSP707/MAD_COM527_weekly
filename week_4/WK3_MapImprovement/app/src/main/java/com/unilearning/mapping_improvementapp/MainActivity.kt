7d3050ef30f544c3d4fd27c7d7989233a4933549
Faizan Ahmed <5ahmef94@solent.ac.uk>
2023-02-17 14:26:48 +0000

    Added the basic code for the map from the previous week, needs updates to work with requirements for week 4's exercises.

--- week_4/WK3_MapImprovement/app/src/main/java/com/unilearning/mapping_improvementapp/MainActivity.kt
+++ week_4/WK3_MapImprovement/app/src/main/java/com/unilearning/mapping_improvementapp/MainActivity.kt
@@ -10,6 +10,13 @@ import org.osmdroid.views.MapView
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        //This line sets the user agent, a requirement to download OSM maps
+        Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this))
+
         setContentView(R.layout.activity_main)
+        val map1 = findViewById<MapView>(R.id.map1)
+        map1.controller.setZoom(14.0)
+        map1.controller.setCenter(GeoPoint(51.05, -0.72))
     }
 }

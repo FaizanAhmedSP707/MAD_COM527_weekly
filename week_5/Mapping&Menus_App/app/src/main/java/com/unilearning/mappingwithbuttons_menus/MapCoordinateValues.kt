64f40c77702549206dfbdb6d30720c237bb8d3e3
Faizan Ahmed <5ahmef94@solent.ac.uk>
2023-02-22 16:38:34 +0000

    Wrote half of the code for submitting values back to the main activity.

--- week_5/Mapping&Menus_App/app/src/main/java/com/unilearning/mappingwithbuttons_menus/MapCoordinateValues.kt
+++ week_5/Mapping&Menus_App/app/src/main/java/com/unilearning/mappingwithbuttons_menus/MapCoordinateValues.kt
@@ -3,8 +3,24 @@ package com.unilearning.mappingwithbuttons_menus
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.Button
 import android.content.Intent
+import android.os.Bundle
+import android.widget.EditText
 import androidx.core.os.bundleOf
 
 class MapCoordinateValues: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_enter_coordinates)
 
+        val latitudeText = findViewById<EditText>(R.id.latitudeField)
+        val longitudeText = findViewById<EditText>(R.id.longitudeField)
+        val submitBtn = findViewById<Button>(R.id.submitValBtn)
+
+        submitBtn.setOnClickListener {
+            /*
+            TODO --> Add some error checking on the fields before accessing data, then call the next
+                     function to send data back to the main activity
+            */
+        }
+    }
 }

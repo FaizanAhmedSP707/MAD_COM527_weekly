f37cd4af5064c8b88840210b72515fe278f2c969
Faizan Ahmed <5ahmef94@solent.ac.uk>
2023-02-22 15:47:24 +0000

    Added all necessary import statements as well as the config for loading the map.

--- week_5/Mapping&Menus_App/app/src/main/java/com/unilearning/mappingwithbuttons_menus/MainActivity.kt
+++ week_5/Mapping&Menus_App/app/src/main/java/com/unilearning/mappingwithbuttons_menus/MainActivity.kt
@@ -2,10 +2,27 @@ package com.unilearning.mappingwithbuttons_menus
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
+import android.view.Menu
+import android.view.MenuItem
+import androidx.activity.result.contract.ActivityResultContracts
+
+import android.preference.PreferenceManager
+import org.osmdroid.config.Configuration
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory
+import org.osmdroid.util.GeoPoint
+import org.osmdroid.views.MapView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this))
+
+        setContentView(R.layout.activity_main)
+        val map1 = findViewById<MapView>(R.id.map1)
+        map1.controller.setZoom(14.0)
+        map1.controller.setCenter(GeoPoint(51.05, -1.4))
     }
 }

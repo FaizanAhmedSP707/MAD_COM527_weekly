7bc1d3614b531798fdaf0e6902be18e789e8f347
Faizan Ahmed <5ahmef94@solent.ac.uk>
2023-02-22 11:27:04 +0000

    Initial commit of the mapping demo, need to add all of the map stuff to the project.

---
+++ week_5/Mapping_Demo_withMaps/app/src/main/java/com/menudemo/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.menudemo
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.activity.result.contract.ActivityResultContracts
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    // This is to inflate the menu (Which is basically loading the menu as Kotlin objects into memory
+    override fun onCreateOptionsMenu(menu: Menu) : Boolean {
+        // In Java we would need to use the getter method called 'getMenuInflater()'
+        // In Kotlin, we can use the property access called 'menuInflater' instead.
+        // Refer to the notes from week 2, which covers some aspects of Kotlin properties
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    // Handle the menu choices
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.choosemap -> {
+                // Launch the secondary activity
+
+                // Create the intent
+                val intent = Intent(this, MapChooseActivity::class.java)
+
+                // Create the launcher
+                val mapChooseLauncher =
+                    registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+                        /*if (it.resultCode == RESULT_OK) {
+                            it.data?.apply {
+                                val opentopomap = this.getBooleanExtra("")
+                            }
+                        }*/
+                        val returnIntent: Intent? = it.data
+                        if (it.resultCode == RESULT_OK) {
+                            returnIntent?.apply {
+                                // "this" refers to returnIntent and it will NOT be null
+                            }
+                        }
+                    }
+
+                return true
+            }
+        }
+        return false
+    }
+}
---
+++ week_5/Mapping_Demo_withMaps/app/src/main/java/com/menudemo/MapChooseActivity.kt
@@ -0,0 +1,11 @@
+package com.menudemo
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MapChooseActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_map_choose)  // Layout for second activity
+    }
+}

afdca7af69e3a0109ac46cacd4146bb8e616650c
Faizan Ahmed <5ahmef94@solent.ac.uk>
2023-02-22 16:25:18 +0000

    Created Kotlin file with it's related layout file.

---
+++ week_5/Mapping&Menus_App/app/src/main/java/com/unilearning/mappingwithbuttons_menus/MapCoordinateValues.kt
@@ -0,0 +1,10 @@
+package com.unilearning.mappingwithbuttons_menus
+
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.Button
+import android.content.Intent
+import androidx.core.os.bundleOf
+
+class MapCoordinateValues: AppCompatActivity() {
+
+}

e506c5141dc2d944b0fa38c454d06b45f8e2fd4e
Faizan Ahmed <5ahmef94@solent.ac.uk>
2023-03-01 11:06:01 +0000

    Added some code to the button listener, as well code for the function that will send back data obtained from the EditText fields.

--- week_5/Mapping&Menus_App/app/src/main/java/com/unilearning/mappingwithbuttons_menus/MapCoordinateValues.kt
+++ week_5/Mapping&Menus_App/app/src/main/java/com/unilearning/mappingwithbuttons_menus/MapCoordinateValues.kt
@@ -21,6 +21,25 @@ class MapCoordinateValues: AppCompatActivity() {
             TODO --> Add some error checking on the fields before accessing data, then call the next
                      function to send data back to the main activity
             */
+            if(latitudeText?.text.toString().trim() == "") {
+                latitudeText.error = "This field shouldn't be empty!"
+            } else if(longitudeText.text.toString().trim() == "") {
+                longitudeText?.error = "This field shouldn't be empty!"
+            } else {
+                val latCoord = latitudeText.text.toString().toDouble()
+                val longCoord = longitudeText.text.toString().toDouble()
+
+                // Now call the function that will send back the relevant data gathered
+                sendBackMapValues(latCoord, longCoord)
+            }
         }
+    }
+
+    fun sendBackMapValues(lat_val: Double, long_val: Double) {
+        val intent = Intent()
+        val bundle = bundleOf("com.unilearning.mappingwithbuttons_menus.latitude" to lat_val, "com.unilearning.mappingwithbuttons_menus.longitude" to long_val)
+        intent.putExtras(bundle)
+        setResult(RESULT_OK, intent)
+        finish()
     }
 }

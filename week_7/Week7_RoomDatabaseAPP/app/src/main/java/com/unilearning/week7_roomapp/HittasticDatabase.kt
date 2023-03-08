5d967e406be7b317968f5aeae781deee6645ae5b
Faizan Ahmed <5ahmef94@solent.ac.uk>
2023-03-08 12:08:41 +0000

    Updated the references to the needed files in order to prevent errors in the compiled app.

--- week_7/Week7_RoomDatabaseAPP/app/src/main/java/com/unilearning/week7_roomapp/HittasticDatabase.kt
+++ week_7/Week7_RoomDatabaseAPP/app/src/main/java/com/unilearning/week7_roomapp/HittasticDatabase.kt
@@ -5,14 +5,14 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = arrayOf(Student::class), version = 1, exportSchema = false)
+@Database(entities = arrayOf(Song::class), version = 1, exportSchema = false)
 public abstract class HittasticDatabase: RoomDatabase() {
-    abstract fun studentDao(): StudentDao
+    abstract fun songDao(): SongDAO
 
     companion object {
-        private var instance: StudentDatabase? = null
+        private var instance: HittasticDatabase? = null
 
-        fun getDatabase(ctx:Context) : StudentDatabase {
+        fun getDatabase(ctx:Context) : HittasticDatabase {
             var tmpInstance = instance
             if(tmpInstance == null) {
                 tmpInstance = Room.databaseBuilder(

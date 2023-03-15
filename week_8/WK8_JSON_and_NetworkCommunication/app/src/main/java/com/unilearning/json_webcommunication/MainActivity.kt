cc3e50db4f802c085917422808eb34d6fa2339bf
Faizan Ahmed <5ahmef94@solent.ac.uk>
2023-03-15 12:39:34 +0000

    Added the necessary import statements.

--- week_8/WK8_JSON_and_NetworkCommunication/app/src/main/java/com/unilearning/json_webcommunication/MainActivity.kt
+++ week_8/WK8_JSON_and_NetworkCommunication/app/src/main/java/com/unilearning/json_webcommunication/MainActivity.kt
@@ -2,6 +2,15 @@ package com.unilearning.json_webcommunication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import com.github.kittinunf.fuel.core.Parameters
+import com.github.kittinunf.fuel.httpGet
+import com.github.kittinunf.fuel.json.responseJson
+import com.github.kittinunf.fuel.gson.responseObject
+import com.github.kittinunf.fuel.httpPost
+import com.github.kittinunf.result.Result
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {

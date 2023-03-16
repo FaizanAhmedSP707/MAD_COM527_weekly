f9d352b1fe6fcc6e1cd7771ca6eae67919cdd105
Faizan Ahmed <5ahmef94@solent.ac.uk>
2023-03-16 21:07:13 +0000

    Added some code that will perform the actions needed for the week's exercises.

--- week_8/WK8_JSON_and_NetworkCommunication/app/src/main/java/com/unilearning/json_webcommunication/MainActivity.kt
+++ week_8/WK8_JSON_and_NetworkCommunication/app/src/main/java/com/unilearning/json_webcommunication/MainActivity.kt
@@ -3,6 +3,7 @@ package com.unilearning.json_webcommunication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import com.github.kittinunf.fuel.core.Parameters
@@ -16,5 +17,39 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val nameEntry = findViewById<EditText>(R.id.songArtistSearchName)
+        val songSearchBtn = findViewById<Button>(R.id.searchSongArtistBtn)
+        val resultView = findViewById<TextView>(R.id.webTextDisplay)
+
+        // Button listener that will send requests to the server
+        songSearchBtn.setOnClickListener {
+
+            // First attempt to access the value entered in the EditText
+            if(nameEntry?.text.toString().trim() == ""){
+                // Right now, we want the user to enter some text into this filed before submitting
+                Toast.makeText(this, "No name entered in the text field above.", Toast.LENGTH_SHORT).show()
+                nameEntry.error = "Artist name field shouldn't be empty."
+            } else {
+                // The EditText isn't empty, so get the value and then send the request to the server
+                val artistName = nameEntry.text.toString()
+
+                val url = "http://10.0.2.2:3000/artist/${artistName}"
+                url.httpGet().response { request, response, result ->
+
+                    when(result) {
+                        is Result.Success -> {
+                            // Parsing not done right now, we'll just be displaying raw data
+                            resultView.text = result.get().decodeToString()
+                        }
+
+                        is Result.Failure -> {
+                            // Failure is when there is an HTTP error
+                            resultView.text = "ERROR ${result.error.message}"
+                        }
+                    }
+                }
+            }
+        }
     }
 }
